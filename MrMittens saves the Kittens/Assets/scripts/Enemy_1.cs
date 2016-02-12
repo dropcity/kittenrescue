25decf12ea081a873aa242ab2952000455cd45c8
Juan Carlos Perez Castellanos <cuyopc@gmail.com>
2016-02-11 21:39:56 -0600

    Adding enemies scripts.
    Scripts for enemy type 1 and 2.

---
+++ MrMittens saves the Kittens/Assets/scripts/Enemy_1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_1 : MonoBehaviour {
+	//This script should be attached to enemy one prefab
+	//If shoot with a bullet, should die
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0,-1*speed,0, Space.World);
+	}
+	//If collides with a bullet, both should be destroyed
+	void OnCollisionEnter(Collision c)
+	{
+		GameObject go = c.gameObject;
+		if (go.layer == 11) {
+			Renderer r = go.GetComponent<Renderer>();
+			//Destroy components
+			Destroy(r);
+			DestroyObject (gameObject);
+		}
+	}
+		
+}
---
+++ MrMittens saves the Kittens/Assets/scripts/Enemy_2.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_2 : MonoBehaviour {
+	//This script should be attached to enemy two prefab
+	//If shoot with a bullet two times, dies
+	//After first shot, duplicates its velocity
+	public float speed;
+	int shot;
+	// Use this for initialization
+	void Start () {
+		shot = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0,speed*-1,0, Space.World);
+	}
+	//If collides with a bullet, both should be destroyed
+	void OnCollisionEnter(Collision c)
+	{
+		GameObject go = c.gameObject;
+		if (go.layer == 11) {
+			Renderer r = go.GetComponent<Renderer> ();
+			if (shot == 1) {				
+				//Destroy components
+				Destroy (r);
+				DestroyObject (gameObject);
+			} 
+			else {
+				Destroy (r);
+				speed = speed * 2;
+				shot++;
+			}
+		}
+	}
+
+}
