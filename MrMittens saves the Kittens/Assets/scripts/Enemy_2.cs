308ba14cdc6489d055b0554e618899a45879d9da
Juan Carlos Perez Castellanos <cuyopc@gmail.com>
2016-02-15 23:08:22 -0600

    Edited enemy 2 script.

--- MrMittens saves the Kittens/Assets/scripts/Enemy_2.cs
+++ MrMittens saves the Kittens/Assets/scripts/Enemy_2.cs
@@ -8,13 +8,15 @@ public class Enemy_2 : MonoBehaviour {
 	public float speed;
 	int shot;
 	// Use this for initialization
+	// Use this for initialization
 	void Start () {
+		StartCoroutine (MoveEnemy());
 		shot = 0;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (0,speed*-1,0, Space.World);
+		//transform.Translate (0,speed*-1,0, Space.World);
 	}
 	//If collides with a bullet, both should be destroyed
 	void OnCollisionEnter(Collision c)
@@ -38,6 +40,14 @@ public class Enemy_2 : MonoBehaviour {
 
 			Renderer r = go.GetComponent<Renderer>();
 			Destroy(r);
+		}
+	}
+
+	IEnumerator MoveEnemy(){
+
+		while (true) {
+			transform.Translate (0, 0, -1 * speed, Space.World);
+			yield return new WaitForSeconds (0.09f);
 		}
 	}
 
